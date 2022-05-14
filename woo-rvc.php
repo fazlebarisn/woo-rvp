86231c091b1f9ca40ce883810fe622d464765086
Fazle Bari <fazlebarisn@gmail.com>
2022-05-14 11:10:52 +0600

    add plugin header

---
+++ woo-rvc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+Plugin Name: Woocommerce recently view product
+Plugin URI: https://www.chitabd.com/
+Description: Display recently view product
+Version: 1.0.0
+Author: Fazle Bari
+Author URI: https://www.chitabd.com/
+Licence: GPLv2 Or leater
+Text Domain: megh
+Domain Path: /languages/
+*/
