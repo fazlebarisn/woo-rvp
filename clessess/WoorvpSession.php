3633440fabb5dbd73ebfd9045e15f012b300def8
Fazle Bari <fazlebarisn@gmail.com>
2022-05-16 22:00:39 +0600

    get current visitor session product

--- clessess/WoorvpSession.php
+++ clessess/WoorvpSession.php
@@ -20,16 +20,44 @@ if(!class_exists('WoorvpSession')){
 
         // Create session name
         public function woorvpSessionName(){
+
             if( is_user_logged_in() ){
                 $user_id = get_current_user_id();
                 $woorvp_session_name = 'woorvp_session' . $user_id;
             }else{
                 $woorvp_session_name = 'woorvp_session_guest';
             }
+
             return $woorvp_session_name;
+
+        }
+
+        // Init session for current user
+        public function woorvpInitSession(){
+
+            $session_name = $this->woorvpSessionName;
+            
+            if( !isset( $_SESSION[$session_name] ) ){
+                $_SESSION[$session_name] = serialize( array() );
+            }
+
+        }
+
+        // get current visitor session product
+        public function woorvpGetProducts(){
+
+            $session_name = $this->woorvpSessionName;
+
+            if( !isset( $_SESSION[$session_name] ) ){
+                return false;
+            }
+
+            return unserialize( $_SESSION[$session_name] );
+            
         }
 
     }
+
     $WoorvpSession = new WoorvpSession;
 
 }
